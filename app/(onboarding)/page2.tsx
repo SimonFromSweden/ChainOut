dd464078db5806fb4bd21e80894d18b9bb8f344a
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-09-30 18:32:17 +0200

    feat: onboarding page 2 design implemented

--- app/(onboarding)/page2.tsx
+++ app/(onboarding)/page2.tsx
@@ -1,5 +1,7 @@
+import OnboardingImage from "@/assets/images/dg-onboarding2.png";
 import PrimaryButton from "@/components/PrimaryButton";
 import { Image, Text, View } from "dripsy";
+import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
 
 export default function OnboardingPage1() {
@@ -13,36 +15,216 @@ export default function OnboardingPage1() {
             alignItems: "center",
             backgroundColor: "#102111",
          }}>
-         <Text
-            sx={{
-               fontSize: 32,
-               fontFamily: "NunitoBold",
-               color: "white",
-               textAlign: "center",
-               marginTop: 80,
-            }}>
-            Welcome to ChainOut
-         </Text>
-         <Text
-            sx={{
-               fontSize: 15,
-               marginTop: 12,
-               color: "$lightGray",
-               paddingHorizontal: 32,
-               textAlign: "center",
-            }}>
-            Your ultimate discgolf companion. Track your scores, unlock
-            achievements, earn badges and compete with friends in a game within
-            the game - ChainOut.
-         </Text>
-         <View sx={{ width: "100%", paddingHorizontal: 16, marginTop: 24 }}>
+         <View sx={{ width: "100%", height: 300, paddingHorizontal: 2 }}>
             <Image
-               source={require("@/assets/images/dg-onboarding3.png")}
-               sx={{
+               source={OnboardingImage}
+               style={{
                   width: "100%",
-                  aspectRatio: 0.66,
-                  borderRadius: 20,
+                  height: "100%",
+                  borderRadius: 8,
+                  overflow: "hidden",
+               }}
+            />
+            {/* Gradient overlay */}
+            <LinearGradient
+               colors={["transparent", "#102111"]} // fade into background color
+               style={{
+                  position: "absolute",
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  height: 140, // how tall the fade is
+               }}
+            />
+            <View
+               sx={{
+                  alignSelf: "flex-start",
+                  paddingHorizontal: 32,
+                  position: "absolute",
+                  bottom: 32,
+               }}>
+               <Text
+                  sx={{
+                     fontSize: 38,
+                     fontFamily: "NunitoBold",
+                     color: "white",
+                     px: 8,
+                     textAlign: "start",
+                  }}>
+                  ChainOut
+               </Text>
+               <Text
+                  sx={{
+                     fontSize: 14,
+                     color: "$lightGray",
+                     px: 8,
+                     textAlign: "start",
+                  }}>
+                  Level up your discgolf game with the ultimate companion app
+                  for tracking scores, chasing badges and connecting with
+                  friends.
+               </Text>
+            </View>
+         </View>
+
+         <View
+            sx={{
+               display: "flex",
+               flexDirection: "row",
+               alignSelf: "flex-start",
+               gap: 8,
+               paddingHorizontal: 20,
+               mt: 12,
+            }}>
+            <Image
+               source={require("@/assets/icons/star-icon-green-30.png")}
+               sx={{
+                  width: 30,
+                  height: 30,
+                  position: "relative",
+                  top: 5,
                }}></Image>
+            <Text
+               sx={{ fontSize: 28, color: "$white", fontFamily: "NunitoBold" }}>
+               Key Features
+            </Text>
+         </View>
+         <Text
+            sx={{
+               fontSize: 13,
+               color: "gray",
+               paddingHorizontal: 24,
+               textAlign: "start",
+            }}>
+            Discover what makes ChainOut the go-to app for discgolfers of all
+            skill-levels.
+         </Text>
+         <View
+            sx={{
+               display: "flex",
+               flexDirection: "col",
+               alignSelf: "start",
+               gap: 32,
+               mt: 32,
+               ml: 42,
+               mb: 14,
+            }}>
+            {/* Feature 1 */}
+            <View sx={{ display: "flex", flexDirection: "row", gap: 12 }}>
+               <Image
+                  source={require("@/assets/images/onboarding-icon1.png")}
+                  sx={{
+                     width: 80,
+                     height: 80,
+                     position: "relative",
+                     top: 4,
+                     borderRadius: 7,
+                  }}></Image>
+               <View
+                  sx={{
+                     display: "flex",
+                     flexDirection: "col",
+                  }}>
+                  <Text
+                     sx={{
+                        fontFamily: "NunitoBold",
+                        fontSize: 16,
+                        color: "$white",
+                        mt: 4,
+                     }}>
+                     Track your scores
+                  </Text>
+                  <Text
+                     sx={{
+                        fontSize: 13,
+                        color: "gray",
+                        textAlign: "start",
+                        maxWidth: 190,
+                        mt: 4,
+                     }}>
+                     Easily record scores, track your achievements and earn
+                     badges in a game within the game!
+                  </Text>
+               </View>
+            </View>
+
+            {/* Feature 2 */}
+            <View sx={{ display: "flex", flexDirection: "row", gap: 12 }}>
+               <Image
+                  source={require("@/assets/images/onboarding-icon2.png")}
+                  sx={{
+                     width: 80,
+                     height: 80,
+                     position: "relative",
+                     top: 4,
+                     borderRadius: 7,
+                  }}></Image>
+               <View
+                  sx={{
+                     display: "flex",
+                     flexDirection: "col",
+                  }}>
+                  <Text
+                     sx={{
+                        fontFamily: "NunitoBold",
+                        fontSize: 16,
+                        color: "$white",
+                        mt: 4,
+                     }}>
+                     Explore Courses
+                  </Text>
+                  <Text
+                     sx={{
+                        fontSize: 13,
+                        color: "gray",
+                        textAlign: "start",
+                        maxWidth: 190,
+                        mt: 4,
+                     }}>
+                     Discover new courses, view detailed maps, and get real-time
+                     course conditions.
+                  </Text>
+               </View>
+            </View>
+
+            {/* Feature 3 */}
+            <View sx={{ display: "flex", flexDirection: "row", gap: 12 }}>
+               <Image
+                  source={require("@/assets/images/onboarding-icon3.png")}
+                  sx={{
+                     width: 80,
+                     height: 80,
+                     position: "relative",
+                     top: 4,
+                     borderRadius: 7,
+                  }}></Image>
+               <View
+                  sx={{
+                     display: "flex",
+                     flexDirection: "col",
+                  }}>
+                  <Text
+                     sx={{
+                        fontFamily: "NunitoBold",
+                        fontSize: 16,
+                        color: "$white",
+                        mt: 4,
+                     }}>
+                     Connect with Players
+                  </Text>
+                  <Text
+                     sx={{
+                        fontSize: 13,
+                        color: "gray",
+                        textAlign: "start",
+                        maxWidth: 190,
+                        mt: 4,
+                     }}>
+                     Join a vibrant community and challenge friends to friendly
+                     badge-collecting competitions.
+                  </Text>
+               </View>
+            </View>
          </View>
          <PrimaryButton
             title="Get Started"
