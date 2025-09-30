86ede8ac0bd4ac94d03b8921cc766d34d0564b67
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-09-30 10:55:26 +0200

    feat: changing to app.config.js instead of app.json (keeping info, but adding api fetching from .env.local file)

---
+++ app.config.js
@@ -0,0 +1,49 @@
+import "dotenv/config";
+
+export default ({ config }) => ({
+   ...config,
+   name: "ChainOut",
+   slug: "ChainOut",
+   version: "1.0.0",
+   orientation: "portrait",
+   icon: "./assets/images/icon.png",
+   scheme: "chainout",
+   userInterfaceStyle: "automatic",
+   newArchEnabled: true,
+   splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+   },
+   ios: {
+      supportsTablet: true,
+      config: {
+         googleMapsApiKey: process.env.GOOGLE_MAPS_IOS_KEY,
+      },
+   },
+   android: {
+      adaptiveIcon: {
+         foregroundImage: "./assets/images/adaptive-icon.png",
+         backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      config: {
+         googleMaps: {
+            apiKey: process.env.GOOGLE_MAPS_ANDROID_KEY,
+         },
+      },
+   },
+   web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+   },
+   plugins: ["expo-router"],
+   experiments: {
+      typedRoutes: true,
+   },
+   extra: {
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+   },
+});
